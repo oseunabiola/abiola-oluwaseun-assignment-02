40fa79a446211cb27f3c3f6a69008b90d7fe7db9
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2022-07-09 06:41:23 +0000

    ADD: Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+	const arrayLength = arrayOfNumbers.length;
+	let sum = 0;
+	for (let i = 0; i < arrayLength; i++) {
+		sum += arrayOfNumbers[i];
+	}
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+	const arrayLength = arrayOfNumbers.length;
+	let evenNumberCount = 0;
+	for (let i = 0; i < arrayLength; i++) {
+		arrayOfNumbers[i] % 2 === 0 && ++evenNumberCount
+	}
+    return evenNumberCount;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
